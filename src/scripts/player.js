1e6dd560075eef5ed7a1d3b0645cebc73c1490e0
dnebik <ya.dneb@yandex.ru>
2022-07-08 13:20:53 +0300

    #3 send request for setting as viewed

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -27,10 +27,17 @@ export function skipIntro() {
   return insertScript('skip_video_intro();');
 }
 
+export function markAsViewed() {
+  return insertScript(`
+    const prev_view_file = 'previously_viewed.php';
+    const cur_p_time = 0;
+    $.get(dle_root + 'engine/ajax/' + prev_view_file + '?the_login_hash=' + the_login_hash + '&pview_id=' + pview_id + '&pview_category=' + pview_category + '&pview_id_seconds=' + cur_p_time + '&mark_as_viewed=true&mark_as_restart=false, (data) => { console.log(data) }');
+  `);
+}
+
 export function onLoaded() {
   return new Promise((resolve, reject) => {
     const vidBlock = document.querySelector('#my-player');
-    console.log(vidBlock);
     if (!vidBlock) {
       reject(new Error('not video page'));
       return;
